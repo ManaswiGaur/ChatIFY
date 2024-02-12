bf7eb055e3837cb6a2f0846f2aa939220e2c1eea
ManaswiGaur <159822925+ManaswiGaur@users.noreply.github.com>
2024-02-13 00:52:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,38 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-// const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 app.use(express.static(__dirname));
-app.get('/',(req,res)=> {
-    res.sendFile(__dirname + '/index.html')
-})
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
 io.on('connection', (socket) => {
     console.log('a user connected');
-    
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 
     socket.on('chat message', (msg) => {
-        console.log('message recive : ',msg)
+        console.log('message received:', msg);
         io.emit('chat message', msg);
     });
 });
 
-server.listen(3000, '192.168.1.7', () => {
-    console.log('listening on 192.168.1.4:3000');
+// Allow cross-origin requests
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://chatify-96bb.onrender.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+const PORT = process.env.PORT || 3000; // Use the port provided by the environment or default to 3000
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
 });
