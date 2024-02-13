77f8e7e84aa4070d5a528afdc0b81437b43e2b2c
ManaswiGaur <159822925+ManaswiGaur@users.noreply.github.com>
2024-02-13 17:03:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Object to store rooms and their participants
+const rooms = {};
+
 app.use(express.static(__dirname));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -14,25 +17,44 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     console.log('a user connected');
 
+    // Handle joining or creating a room
+    socket.on('joinOrCreateRoom', (roomCode) => {
+        if (!rooms[roomCode]) {
+            rooms[roomCode] = [];
+            console.log(`Room created: ${roomCode}`);
+        }
+        socket.join(roomCode);
+        rooms[roomCode].push(socket.id);
+        console.log(`User joined room: ${roomCode}`);
+        // Emit an event to notify client about successful room join
+        io.to(socket.id).emit('roomJoined', roomCode);
+    });
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
+        // Find and remove the disconnected user from all rooms
+        for (const roomCode in rooms) {
+            const index = rooms[roomCode].indexOf(socket.id);
+            if (index !== -1) {
+                rooms[roomCode].splice(index, 1);
+                console.log(`User left room: ${roomCode}`);
+                // If no users left in the room, delete the room
+                if (rooms[roomCode].length === 0) {
+                    delete rooms[roomCode];
+                    console.log(`Room deleted: ${roomCode}`);
+                }
+                break;
+            }
+        }
     });
 
-    socket.on('chat message', (msg) => {
-        console.log('message received:', msg);
-        io.emit('chat message', msg);
+    socket.on('chat message', (data) => {
+        // Broadcast the message to everyone in the room
+        io.sockets.in(data.room).emit('chat message',data);
     });
 });
 
-// Allow cross-origin requests
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://chatify-96bb.onrender.com');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
-const PORT = process.env.PORT || 3000; // Use the port provided by the environment or default to 3000
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
